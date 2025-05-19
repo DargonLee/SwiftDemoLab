2ffc83669b08b9c4665f25dfccadff54ffa75bdd
harlans <2461414445@qq.com>
2025-05-19 17:42:33 +0800

    init

---
+++ SwiftDemoLabTests/SwiftDemoLabTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDemoLabTests.swift
+//  SwiftDemoLabTests
+//
+//  Created by lihailong on 2025/5/19.
+//
+
+import Testing
+@testable import SwiftDemoLab
+
+struct SwiftDemoLabTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
