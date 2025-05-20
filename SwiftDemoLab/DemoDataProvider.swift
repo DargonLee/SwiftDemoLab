a419813c07b5d8cc78d02ae2a2ab145bfe12aa39
harlans <2461414445@qq.com>
2025-05-20 11:28:45 +0800

    搭建主页面架构

---
+++ SwiftDemoLab/DemoDataProvider.swift
@@ -0,0 +1,31 @@
+//
+//  DemoDataProvider.swift
+//  SwiftDemoLab
+//
+//  Created by lihailong on 2025/5/20.
+//
+
+import UIKit
+
+struct DemoItem {
+    let title: String
+    let desc: String
+    let controllerType: UIViewController.Type
+}
+
+struct DemoSection {
+    let title: String
+    let items: [DemoItem]
+}
+
+struct DemoDataProvider {
+    static func allSections() -> [DemoSection] {
+        return [
+            DemoSection(title: "功能演示", items: [
+                DemoItem(title: "测试页面", desc: "TestViewController 示例", controllerType: TestViewController.self)
+                // 这里可以继续添加更多 Demo
+            ])
+            // 可以继续添加更多 Section
+        ]
+    }
+}
--- SwiftDemoLab/MainViewController.swift
+++ SwiftDemoLab/MainViewController.swift
@@ -5,28 +5,70 @@
 //  Created by lihailong on 2025/5/19.
 //
 
-import UIKit
 import Inject
+import UIKit
 
 #if DEBUG
-extension UIViewController {
-    @objc func injected() {
-        viewDidLoad()
+    extension UIViewController {
+        @objc func injected() {
+            viewDidLoad()
+        }
     }
-}
 #endif
 
 class MainViewController: UIViewController {
+    private var tableView: UITableView!
+    private var dataSource: [DemoSection] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "SwiftDemoLab"
+        view.backgroundColor = .systemBackground
+        setupTableView()
+        setupDataSource()
     }
 
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        let viewController = Inject.ViewControllerHost(TestViewController())
-        navigationController?.pushViewController(viewController, animated: true)
+    private func setupTableView() {
+        tableView = UITableView(frame: view.bounds, style: .insetGrouped)
+        tableView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        tableView.delegate = self
+        tableView.dataSource = self
+        view.addSubview(tableView)
     }
-    
+
+    private func setupDataSource() {
+        dataSource = DemoDataProvider.allSections()
+        tableView.reloadData()
+    }
 }
 
+extension MainViewController: UITableViewDelegate, UITableViewDataSource {
+    // MARK: - UITableViewDataSource
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return dataSource.count
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource[section].items.count
+    }
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return dataSource[section].title
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") ??
+            UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        let item = dataSource[indexPath.section].items[indexPath.row]
+        cell.textLabel?.text = item.title
+        cell.detailTextLabel?.text = item.desc
+        cell.accessoryType = .disclosureIndicator
+        return cell
+    }
+    // MARK: - UITableViewDelegate
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let item = dataSource[indexPath.section].items[indexPath.row]
+        let vc = item.controllerType.init()
+        vc.title = item.title
+        let hostVC = Inject.ViewControllerHost(vc)
+        navigationController?.pushViewController(hostVC, animated: true)
+    }
+}
